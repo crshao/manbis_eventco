ca8d7e014d8977e088e322ce22ddb87a3e3335c7
hao <calvindon08@gmail.com>
2019-11-24 15:51:52 +0700

    update

--- app/src/main/java/com/example/manbis_eventco/RegisterActivity.java
+++ app/src/main/java/com/example/manbis_eventco/RegisterActivity.java
@@ -1,14 +1,35 @@
 package com.example.manbis_eventco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.OnClick;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private final static String TAG = "RegitsterActivity";
+
+    @BindView(R.id.)
+
+    @BindView(R.id.)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @OnClick()
+    void register()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences("pref", MODE_PRIVATE);
+        String newUser;
+        String newPass;
+        String newPhone;
+        String newEmail;
     }
 }
