2c98fc6b68345699ca55617e7e378158860c04b5
Bagas Immanuel <bagasimmanuel00@gmail.com>
2019-11-27 17:24:15 +0700

    Filter_Vendor

---
+++ app/src/main/java/com/example/manbis_eventco/Filter_Vendor.java
@@ -0,0 +1,14 @@
+package com.example.manbis_eventco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Filter_Vendor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter__vendor);
+    }
+}
