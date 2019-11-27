37b9a3b0cfe3c935fa05f3e6c8679cc6b879d077
hao <calvindon08@gmail.com>
2019-11-27 23:32:22 +0700

    Wedding Occasion sudah bisa di view

--- app/src/main/java/com/example/manbis_eventco/EventList.java
+++ app/src/main/java/com/example/manbis_eventco/EventList.java
@@ -1,14 +1,30 @@
 package com.example.manbis_eventco;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+
 public class EventList extends AppCompatActivity {
+
+    @BindView(R.id.btn_wedding)
+    MaterialButton btn_wedding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_wedding)
+    public void intent()
+    {
+        Intent intent = new Intent();
     }
 }
