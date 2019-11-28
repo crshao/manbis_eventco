c29ba2452deb8b015820edeb562a1fa34df40d81
Bagas Immanuel <bagasimmanuel00@gmail.com>
2019-11-28 12:42:24 +0700

    Payment Method & Payment

---
+++ app/src/main/java/com/example/manbis_eventco/FiturPayment.java
@@ -0,0 +1,14 @@
+package com.example.manbis_eventco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiturPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitur_payment);
+    }
+}
