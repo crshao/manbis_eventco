bf51590e9a3208943461304ebdfdf3552f16771c
hao <calvindon08@gmail.com>
2019-11-28 21:07:25 +0700

    Venue

---
+++ app/src/main/java/com/example/manbis_eventco/Adapter/VenueResultAdapter.java
@@ -0,0 +1,4 @@
+package com.example.manbis_eventco.Adapter;
+
+public class VenueResultAdapter {
+}
---
+++ app/src/main/java/com/example/manbis_eventco/ListVenueResult.java
@@ -0,0 +1,14 @@
+package com.example.manbis_eventco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListVenueResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_venue_result);
+    }
+}
