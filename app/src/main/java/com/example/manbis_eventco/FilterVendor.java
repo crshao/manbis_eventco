5fdf6a1da0fb95b41dd9a4dacf814ac978b69c41
hao <calvindon08@gmail.com>
2019-11-30 20:33:50 +0700

    button_search

--- app/src/main/java/com/example/manbis_eventco/FilterVendor.java
+++ app/src/main/java/com/example/manbis_eventco/FilterVendor.java
@@ -5,27 +5,42 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.manbis_eventco.Adapter.WeddingOccasionAdapter;
 import com.example.manbis_eventco.Data.WeddingOccasion;
 import com.example.manbis_eventco.Data.WeddingOccasionData;
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
 public class FilterVendor extends AppCompatActivity {
 
+    @BindView(R.id.button_search)
+    MaterialButton button_search;
+
+    Bundle bundle;
+    String data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_vendor);
-        Bundle bundle = getIntent().getExtras();
-        String data = bundle.getString("data");
+        ButterKnife.bind(this);
+        bundle = getIntent().getExtras();
+        data = bundle.getString("data");
 
         Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.button_search)
+    public void move()
+    {
         Intent intent = new Intent(this, FilterResult.class);
         intent.putExtra("data", data);
         startActivity(intent);
