e901390d4de00bb7dd3441531b36ccd6015655ed
Bagas Immanuel <bagasimmanuel00@gmail.com>
2019-12-01 19:03:14 +0700

    RecycleView Spacing

--- app/src/main/java/com/example/manbis_eventco/RecycleViewSpacing.java
+++ app/src/main/java/com/example/manbis_eventco/RecycleViewSpacing.java
@@ -1,6 +1,20 @@
 package com.example.manbis_eventco;
 
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecycleViewSpacing extends RecyclerView.ItemDecoration {
+
+    private final int verticalSpace;
+
+    public RecycleViewSpacing(int verticalSpace) {
+        this.verticalSpace = verticalSpace;
+    }
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        outRect.bottom = verticalSpace;
+    }
 }
